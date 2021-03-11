d86c029daa6b44a44f9e988a0377ba281ac5974d
amirtorkama5204@gmail.com <amirtorkaman5204@gmail.com>
2021-03-11 13:14:57 +0330

    Add server streaming greet

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/amir2539/grpc/greet/greetpb"
 	"google.golang.org/grpc"
+	"io"
 	"log"
 )
 
@@ -19,7 +20,8 @@ func main() {
 	defer cc.Close()
 	c := greetpb.NewGreetServiceClient(cc)
 
-	doUnary(c)
+	//doUnary(c)
+	doServerStreaming(c)
 }
 
 func doUnary(c greetpb.GreetServiceClient) {
@@ -36,4 +38,34 @@ func doUnary(c greetpb.GreetServiceClient) {
 	}
 
 	log.Printf("Response from greet %v", res)
+}
+
+func doServerStreaming(c greetpb.GreetServiceClient) {
+	fmt.Println("Client many")
+
+	req := &greetpb.GreetManyTimesRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Amir",
+			LastName:  "Torkaman",
+		},
+	}
+
+	resStream, err := c.GreetManyTimes(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error %v", err)
+	}
+
+	for {
+		msg, err := resStream.Recv()
+		if err == io.EOF {
+			// Server closed stream
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Error Stream %v", err)
+		}
+
+		log.Printf("Result from manytimes %v", msg.GetResult())
+	}
 }
