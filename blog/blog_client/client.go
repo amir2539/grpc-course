3cf88f927b2100955b56ed7f625ab9dfa10a3395
amirtorkama5204@gmail.com <amirtorkaman5204@gmail.com>
2021-05-01 23:16:27 +0430

    Add update, delete and list blog

--- blog/blog_client/client.go
+++ blog/blog_client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/amir2539/grpc/blog/blogpb"
 	"google.golang.org/grpc"
+	"io"
 	"log"
 )
 
@@ -20,7 +21,10 @@ func main() {
 	c := blogpb.NewBlogServiceClient(cc)
 
 	//createBlog(c)
-	readBlog(c)
+	//readBlog(c)
+	//updateBlog(c)
+	//deleteBlog(c)
+	listBlogs(c)
 }
 func readBlog(c blogpb.BlogServiceClient) {
 	fmt.Println("Reading blog")
@@ -38,9 +42,9 @@ func readBlog(c blogpb.BlogServiceClient) {
 func createBlog(c blogpb.BlogServiceClient) {
 	fmt.Println("Creating blog")
 	blog := &blogpb.Blog{
-		AuthorId: "Stephane",
-		Title:    "One",
-		Content:  "First content",
+		AuthorId: "hgfb",
+		Title:    "tr3",
+		Content:  "سلام ایرانی",
 	}
 	res, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{
 		Blog: blog,
@@ -50,4 +54,55 @@ func createBlog(c blogpb.BlogServiceClient) {
 	}
 
 	fmt.Println("Blog has been created %v", res)
+}
+
+func updateBlog(c blogpb.BlogServiceClient) {
+	fmt.Println("Creating blog")
+	blog := &blogpb.Blog{
+		Id:       "6086ffdd4e6d91ce1488d041",
+		AuthorId: "author changed",
+		Title:    "One (edited)",
+		Content:  "First content 2",
+	}
+	res, err := c.UpdateBlog(context.Background(), &blogpb.UpdateBlogRequest{
+		Blog: blog,
+	})
+	if err != nil {
+		log.Fatalf("Unexpected eror %v", err)
+	}
+
+	fmt.Println("Blog has been updated %v", res)
+}
+
+func deleteBlog(c blogpb.BlogServiceClient) {
+	fmt.Println("Deleting blog")
+
+	_, err := c.DeleteBlog(context.Background(), &blogpb.DeleteBlogRequest{BlogId: "6086ffdd4e6d91ce1488d041"})
+	if err != nil {
+		log.Printf("Error %v", err)
+		return
+	}
+
+	fmt.Printf("Blog has been deleted")
+}
+
+func listBlogs(c blogpb.BlogServiceClient) {
+
+	stream, err := c.ListBlog(context.Background(), &blogpb.ListBlogRequest{})
+	if err != nil {
+		log.Fatalf("Error while calling rpc %v", err)
+	}
+
+	for {
+		response, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Error while reading %v", err)
+		}
+
+		fmt.Println(response.GetBlog())
+	}
 }
--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -10,6 +10,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc/status"
 	"log"
 	"net"
@@ -27,6 +28,15 @@ type blogItem struct {
 	AuthorId string             `bson:"author_id"`
 	Content  string             `bson:"content"`
 	Title    string             `bson:"title"`
+}
+
+func dataToBlogpb(data *blogItem) *blogpb.Blog {
+	return &blogpb.Blog{
+		Id:       data.ID.Hex(),
+		AuthorId: data.AuthorId,
+		Title:    data.Title,
+		Content:  data.Content,
+	}
 }
 
 func (*server) CreateBlog(ctx context.Context, req *blogpb.CreateBlogRequest) (*blogpb.CreateBlogResponse, error) {
@@ -74,12 +84,90 @@ func (*server) ReadBlog(ctx context.Context, req *blogpb.ReadBlogRequest) (*blog
 		return nil, status.Errorf(codes.NotFound, fmt.Sprintf("CAnnot find blog"))
 	}
 
-	return &blogpb.ReadBlogResponse{Blog: &blogpb.Blog{
-		Id:       data.ID.Hex(),
-		AuthorId: data.AuthorId,
-		Title:    data.Title,
-		Content:  data.Content,
-	}}, nil
+	return &blogpb.ReadBlogResponse{Blog: dataToBlogpb(data)}, nil
+}
+
+func (*server) UpdateBlog(ctx context.Context, req *blogpb.UpdateBlogRequest) (*blogpb.UpdateBlogResponse, error) {
+	blog := req.GetBlog()
+	fmt.Println("Update blog started")
+	oid, err := primitive.ObjectIDFromHex(blog.GetId())
+	if err != nil {
+		return nil, status.Errorf(
+			codes.InvalidArgument, fmt.Sprintf("invalid argument"))
+	}
+
+	data := &blogItem{}
+	filter := bson.D{
+		bson.E{Key: "_id", Value: oid},
+	}
+
+	result := collection.FindOne(context.Background(), filter)
+	if err := result.Decode(data); err != nil {
+		return nil, status.Errorf(codes.NotFound, fmt.Sprintf("CAnnot find blog"))
+	}
+
+	data.AuthorId = blog.GetAuthorId()
+	data.Content = blog.GetContent()
+	data.Title = blog.GetTitle()
+
+	_, UpdateError := collection.ReplaceOne(context.Background(), filter, data)
+	if UpdateError != nil {
+		return nil, status.Errorf(
+			codes.Internal, fmt.Sprintf("Cannot update %v", UpdateError))
+	}
+
+	return &blogpb.UpdateBlogResponse{Blog: dataToBlogpb(data)}, nil
+
+}
+
+func (*server) DeleteBlog(ctx context.Context, req *blogpb.DeleteBlogRequest) (*blogpb.DeleteBlogResponse, error) {
+	blogId := req.GetBlogId()
+	oid, err := primitive.ObjectIDFromHex(blogId)
+	if err != nil {
+		return nil, status.Errorf(
+			codes.InvalidArgument, fmt.Sprintf("invalid argument"))
+	}
+
+	filter := bson.D{
+		bson.E{Key: "_id", Value: oid},
+	}
+	deleteResult, deleteError := collection.DeleteOne(context.Background(), filter)
+	if deleteError != nil {
+		return nil, status.Errorf(codes.Internal, fmt.Sprintf("Cant delete blog %v", deleteError))
+	}
+
+	if deleteResult.DeletedCount == 0 {
+		return nil, status.Errorf(codes.NotFound, fmt.Sprintf("cannot find blog"))
+	}
+
+	return &blogpb.DeleteBlogResponse{}, nil
+
+}
+
+func (*server) ListBlog(req *blogpb.ListBlogRequest, stream blogpb.BlogService_ListBlogServer) error {
+
+	fmt.Println("List Blog")
+
+	cursor, err := collection.Find(context.Background(), bson.D{{}})
+	if err != nil {
+		return status.Errorf(codes.Internal, fmt.Sprintf("Cant adad blog %v", err))
+	}
+	defer cursor.Close(context.Background())
+
+	for cursor.Next(context.Background()) {
+		data := &blogItem{}
+		err := cursor.Decode(data)
+		if err != nil {
+			return status.Errorf(codes.Internal, fmt.Sprintf("Cant decode blog %v", err))
+		}
+
+		stream.Send(&blogpb.ListBlogResponse{Blog: dataToBlogpb(data)})
+	}
+	if err := cursor.Err(); err != nil {
+		return status.Errorf(codes.Internal, fmt.Sprintf("unknown %v", err))
+	}
+
+	return nil
 }
 
 func main() {
@@ -107,6 +195,7 @@ func main() {
 	s := grpc.NewServer()
 	blogpb.RegisterBlogServiceServer(s, &server{})
 
+	reflection.Register(s)
 	go func() {
 		fmt.Println("start server")
 		if err := s.Serve(lis); err != nil {
